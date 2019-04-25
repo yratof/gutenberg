c10bcab33aed189c84e3e5bf48104496891bd9e2
Andrew Duthie <andrew@andrewduthie.com>
2019-04-25 13:28:51 -0400

    Data: Documentation: Use JSDoc see tag in place of link

--- packages/data/src/components/registry-provider/index.js
+++ packages/data/src/components/registry-provider/index.js
@@ -14,7 +14,7 @@ const { Consumer, Provider } = createContext( defaultRegistry );
  * A context consumer component which provides to the rendered child function a
  * reference to the current registry context.
  *
- * @link https://reactjs.org/docs/context.html#contextconsumer
+ * @see https://reactjs.org/docs/context.html#contextconsumer
  *
  * @type {WPComponent}
  */
@@ -24,7 +24,7 @@ export const RegistryConsumer = Consumer;
  * A context provider component which provides to descendent RegistryConsumer
  * elements a reference to the given registry context.
  *
- * @link https://reactjs.org/docs/context.html#contextprovider
+ * @see https://reactjs.org/docs/context.html#contextprovider
  *
  * @type {WPComponent}
  */
